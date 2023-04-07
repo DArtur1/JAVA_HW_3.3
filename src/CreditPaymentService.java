1f90ffc93b297cab662fccb21f4714b39fbcba54
DudkaAV_QA <arturdudka@yandex.ru>
2023-04-08 00:32:22 +0700

    Изменение формулы расчета interestRate

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,8 @@
 public class CreditPaymentService {
-    public int calculate (int summaCredit, int numberMonth, double yearStavka) {
-        double procStavka = (double) (yearStavka / numberMonth / 100);
-        double kof = (double)(procStavka * (Math.pow(1 + procStavka, numberMonth)) / ((Math.pow(1 + procStavka, numberMonth)) - 1));
-        int itog = (int) (kof * summaCredit);
-        return itog;
+    public int calculate (int amountCredit, int numberMonth, double yearRate) {
+        double interestRate = (double) (yearRate / 12 / 100);
+        double annuityRat = (double)(interestRate * (Math.pow(1 + interestRate, numberMonth)) / ((Math.pow(1 + interestRate, numberMonth)) - 1));
+        int monthPay = (int) (annuityRat * amountCredit);
+        return monthPay;
     }
 }
